4a80f6f4f5767fe474b09b4fe8569f0661b00869
Celso Maçao Saito Neto <cmassao@gmail.com>
2021-04-02 12:25:18 -0500

    Create Enumeration to Represent Product Rating

--- src/labs/pm/data/Product.java
+++ src/labs/pm/data/Product.java
@@ -20,28 +20,31 @@ import java.math.BigDecimal;
 import static java.math.RoundingMode.HALF_UP;
 
 /**
- * {@code Product} class represents properties and behaviours of
- * product objects in the Product Management System.
+ * {@code Product} class represents properties and behaviours of product objects
+ * in the Product Management System.
  * <br>
  * Each product has an id, name and price
  * <br>
  * Each product can have a discount, calculated based on a
  * {@link  DISCOUNT_RATE discount rate}
+ *
  * @version 4.0
  * @author cmass
  */
 public class Product {
+
     /**
-     * A constant that define a 
-     * {@link java.math.BigDecimal BigDecimal} value of the discount rate
+     * A constant that define a {@link java.math.BigDecimal BigDecimal} value of
+     * the discount rate
      * <br>
      * Discount rate is 10%
      */
     public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
-    
+
     private int id;
     private String name;
     private BigDecimal price;
+    private Rating rating;
 
     public int getId() {
         return id;
@@ -67,14 +70,18 @@ public class Product {
 //        price = BigDecimal.ONE;
         this.price = price;
     }
-    
+
     /**
      * Calculates discount based on a product price and
      * {@link DISCOUNT_RATE discount rate}
-     * @return a {@link java.math.BigDecimal BigDecimal}
-     * value of the discount
+     *
+     * @return a {@link java.math.BigDecimal BigDecimal} value of the discount
      */
-    public BigDecimal getDiscount(){
-        return price.multiply(DISCOUNT_RATE).setScale(2,HALF_UP);
+    public BigDecimal getDiscount() {
+        return price.multiply(DISCOUNT_RATE).setScale(2, HALF_UP);
+    }
+
+    public Rating getRating() {
+        return rating;
     }
 }
