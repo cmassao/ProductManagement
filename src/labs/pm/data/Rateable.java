703604de72dff1034a7b9413e35d23737ac363e4
Celso Maçao Saito Neto <cmassao@gmail.com>
2021-04-03 17:36:39 -0500

    Design the Rateable Interface

---
+++ src/labs/pm/data/Rateable.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labs.pm.data;
+
+/**
+ *
+ * @author cmass
+ */
+@FunctionalInterface
+public interface Rateable<T> {
+    public static final Rating DEFAULT_RATING = Rating.NOT_RATED;
+    
+    T applyRating(Rating rating);
+    
+    public default T applyRating(int stars){
+        return applyRating(convert(stars));
+    }
+    
+    public default Rating getRating(){
+        return DEFAULT_RATING;
+    }
+    
+    public static Rating convert(int stars){
+        return (stars>=0&&stars<=0) ? Rating.values()[stars] : DEFAULT_RATING;
+    }
+}
